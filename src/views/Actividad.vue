7997da9e854eca0c5f958b460ae1edead0a2de0f
jhonatang08 <111087270+jhonatang08@users.noreply.github.com>
2022-12-14 17:20:50 -0500

    ajustes

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -17,7 +17,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Tipo de actividad
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/Actividad_1/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
