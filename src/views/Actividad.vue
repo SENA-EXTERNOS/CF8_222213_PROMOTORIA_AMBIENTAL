700addbe6e4bcb3b16b97c7725b1739acb3f706c
jhonatang08 <111087270+jhonatang08@users.noreply.github.com>
2022-12-19 21:40:50 -0500

    ajuste

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -15,7 +15,7 @@
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Tipo de actividad
+              p.fw-bold.mb-0 Relación de conceptos 
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/Actividad_1/index.html')" target="_blank")
                 span Realizar
